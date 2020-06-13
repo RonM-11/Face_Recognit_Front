a36ccd0140603cb98afe31dc59095ee9a345b342
Ron <rongalron@gmail.com>
2020-06-13 18:09:27 +0300

    Updates

---
+++ precache-manifest.c44b4f1558079a89a69b4a432fef29ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d3afd51d025bc273806a1721eb148d1",
+    "url": "/Face_Recognit_Front/index.html"
+  },
+  {
+    "revision": "d791592186febd84fc86",
+    "url": "/Face_Recognit_Front/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "116e18c0b184616936bf",
+    "url": "/Face_Recognit_Front/static/css/main.b2aee1ee.chunk.css"
+  },
+  {
+    "revision": "d791592186febd84fc86",
+    "url": "/Face_Recognit_Front/static/js/2.2b5adc06.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Face_Recognit_Front/static/js/2.2b5adc06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "116e18c0b184616936bf",
+    "url": "/Face_Recognit_Front/static/js/main.a1e76232.chunk.js"
+  },
+  {
+    "revision": "a7f9eec9985fe5e8011e",
+    "url": "/Face_Recognit_Front/static/js/runtime-main.e88b7beb.js"
+  }
+]);
